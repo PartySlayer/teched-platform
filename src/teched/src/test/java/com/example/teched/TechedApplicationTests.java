d27a15c3f2f95451e085cc04cb041e26c305f3e5
shadow <mauroripa7@gmail.com>
2026-01-29 21:30:06 +0100

    import springboot project

---
+++ src/teched/src/test/java/com/example/teched/TechedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teched;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
