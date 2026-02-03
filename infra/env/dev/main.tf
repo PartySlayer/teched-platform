c3c722c71c232405b8f0de66b621c4f08b3156d3
PartySlayer <squadrittichristian@gmail.com>
2026-02-03 03:23:04 +0100

    terraform apply di ecs cluster e vpc senza errori

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -17,3 +17,10 @@ module "networking" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.10.0/24", "10.0.11.0/24"]
 }
+
+module "ecs_cluster" {
+  source = "../../modules/ecs-cluster"
+
+  cluster_name = var.project_name
+
+}
