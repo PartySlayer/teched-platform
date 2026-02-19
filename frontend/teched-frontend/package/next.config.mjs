034b3a3724788d2a4acb6b59ca488b047fe3e12a
shadow <mauroripa7@gmail.com>
2026-02-19 21:50:03 +0100

    - fixed api issue with backend

--- frontend/teched-frontend/package/next.config.mjs
+++ frontend/teched-frontend/package/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/:path*', // Reindirizza al backend Spring Boot
+        destination: 'http://localhost:8080/api/:path*', // Reindirizza al backend Spring Boot
       },
     ];
   },
